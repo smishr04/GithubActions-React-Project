0fd01bb43aa153b8fa917ad745043b0da9d75a66
Sanjiv <sanjiv04mishra@gmail.com>
2023-03-18 11:57:37 +0530

    custom js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
 const core= require('@actions/core')
-const core= require('@actions/exec')
-const core= require('@actions/github')
+const exec= require('@actions/exec')
+const github= require('@actions/github')
 
 function run() {
   core.notice('Hello from custom JS Action')
